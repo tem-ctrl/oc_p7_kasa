afbb2cd30cbdf36eeb981378750290d1ee99d633
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-15 14:49:13 +0100

    creat app context with houses data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-// import { useState, useEffect } from 'react'
+import { createContext } from 'react'
 import Banner from '../components/Banner'
 import Gallery from '../components/Gallery'
+import { houses } from '../data/logements'
 
+export const AppContext = createContext(houses)
 
 function Home() {
   const title = document.querySelector('title')
