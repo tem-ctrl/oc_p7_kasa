a06b2b97f94859a2ff102fea11810c8b41fcf831
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-15 14:51:05 +0100

    add open/close effect

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import Tag from './Tag'
+import upArrow from '../assets/arrow-up.png'
 
 function Dropdown(props) {
 
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div>
-      <Tag title={props.title} isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
-      <p className='light-bg'>{props.description}</p>
+    <div className='dropdown light-bg'>
+      <div className='dropdown__head' onClick={() => setIsOpen(!isOpen)}>
+        <p className='white'>{props.title}</p>
+        <img
+          src={upArrow}
+          alt={isOpen ? "flèche vers le haut" : "flèche vers le bas"}
+          className={isOpen ? "dropdown__img" : "dropdown__img dropdown__img__rotated"}
+        />
+      </div>
+      <p className={isOpen ? 'dropdown__body light-bg ' : 'dropdown__body dropdown__body--closed light-bg'}>
+        {props.description}
+      </p>
     </div>
   )
 }
