43f2e5d57e68a508533a2fd8d4efb7460b652156
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-16 13:41:42 +0100

    update line height

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card(props) {
   return (
-    <article className="article card" key={props.key}>
+    <article className="card" key={props.key}>
       <img className="card__img" src={props.image} alt={props.title} />
       <h2 className="h2 card__title white">{props.title}</h2>
     </article>
