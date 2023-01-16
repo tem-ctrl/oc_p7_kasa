183b43d6c196a4f1fa565eb44b95a4f0c1ea5860
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-16 20:19:45 +0100

    remove file

--- src/Contexts.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react"
-import { houses } from './data/logements'
-
-export const AppContext = createContext(null)
-
-export const makeHouseContext = (houseId) => {
-  const house = houses.find(elt => elt.id === houseId)
-  return createContext(house)
-}
