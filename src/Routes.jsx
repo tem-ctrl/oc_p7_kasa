6542357448f7e6f6a17a3ca2fcdfff4970fbda52
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-16 20:12:17 +0100

    add scrollToTop

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,19 +5,22 @@ import Error from './pages/Error'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import House from './pages/House'
+import ScrollToTop from './ScrollToTop'
 
 const MyRoutes = (
   <Router>
-    <Header />
-    <main className='main'>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/house/:houseId' element={<House />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-    </main>
-    <Footer />
+    <ScrollToTop>
+      <Header />
+      <main className='main'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/house/:houseId' element={<House />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </main>
+      <Footer />
+    </ScrollToTop>
   </Router>
 )
 
