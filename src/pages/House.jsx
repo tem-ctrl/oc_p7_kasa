5889627c1bb6e2045b3cbc87bd5cdecd592ac4c4
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-20 08:43:52 +0100

    create Housebody component

--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -3,50 +3,34 @@ import { useParams } from 'react-router-dom'
 import Carousel from '../components/Carousel'
 import Error from '../pages/Error'
 import { AppContext } from './Home'
-import Tag from '../components/Tag'
-import Host from '../components/Host'
-import Dropdown from '../components/Dropdown'
-import Rating from '../components/Rating'
+import HouseBody from '../components/HouseBody'
 
-
-export const houseContext = createContext(null) // Context to provide to its own children
+// Context to provide to its children
+export const houseContext = createContext(null)
 
 function House() {
-  const houses = useContext(AppContext) // Using context from home page
-  const { houseId } = useParams()
-  const house = houses.find((elt) => elt.id === houseId)
 
+  // Extract house id from url  
+  const { houseId } = useParams()
+
+  // Find house by id from AppContext
+  const house = useContext(AppContext).find((elt) => elt.id === houseId)
+
+  // Return Error Page if house don't exist
   if (!house) {
     return (
       <Error />
     )
   } else {
+    // Set the page title
     const title = document.querySelector('title')
     title.textContent = `Kasa - ${house.title}`
 
     return (
+      // Provide house data to its children through context
       <houseContext.Provider value={house}>
         <Carousel />
-        <section className="house">
-          <div className="house__head">
-            <h1 className="house__title">{house.title}</h1>
-            <h2 className="house__location">{house.location}</h2>
-            <div className="house__tags">
-              {house.tags.map((tag) => (
-                <Tag key={tag} name={tag} />
-              ))}
-            </div>
-          </div>
-          <div className="house__host">
-            <Host />
-            <Rating />
-          </div>
-        </section>
-
-        <div className='house__desc'>
-          <Dropdown title="Description" body={house.description} />
-          <Dropdown title="Équipements" body={house.equipments} />
-        </div>
+        <HouseBody />
       </houseContext.Provider>
     )
   }
