9268d0ba9b26b201404b91fbef45ebc069b7dba8
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-01-31 07:37:46 +0100

    move error page title to Error component

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,6 @@
 import { Error } from '../components'
 
 function ErrorPage() {
-  // Set page title
-  const title = document.querySelector('title')
-  title.textContent = 'Page inexistante'
-
   return (
     <Error />
   )
